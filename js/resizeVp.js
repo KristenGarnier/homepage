15b9e4ac870cb3c1de2e7b331c41e57a5ca03e3b
KristenGarnier <garnier.kristen@icloud.com>
2015-07-26 23:26:25 +0200

    Finition de la vue

--- js/resizeVp.js
+++ js/resizeVp.js
@@ -5,29 +5,48 @@ $(document).ready(function () {
     var height = windowSet();
     if($target){
         fondSet(height, $target);
+        voyagesSet(height);
     }
 
     // quand on redimensionne la fenetre
     $(window).resize(function(){
         height = windowSet();
         if($target){
-            fondSet(height);
+            console.log(height);
+            fondSet(height, $target);
+            voyagesSet(height)
         }
-    })
+    });
 
-});
+    function fondSet(height, $target){
 
-// fonction pour centrer le formulaire d'inscription [non  terminée ]
-function fondSet(height, $target){
+        $target.height(height);
 
-    $target.height(height);
-
-}
+    }
 
 
 // fonction pour que le fond occupe toute la page
-function windowSet(){
-    var height = $(window).height();
+    function windowSet(){
+        var height = $(window).height();
 
-    return height;
-}
+        return height;
+    }
+
+    function voyagesSet(height) {
+
+        console.log(height, $('.resultcontent').height());
+
+        if(height < $('.resultcontent').height()){
+
+            console.log('in');
+            $('#researchBigPicture').height( $('.resultcontent').height() + 200);
+
+            return false;
+        }else {
+            $('#researchBigPicture').height( height);
+        }
+
+        return true;
+    }
+
+});
